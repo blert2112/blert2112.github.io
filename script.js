80d4d840a6e12ba4bf3fb0c285beb78953d24e1f
blert2112 <blert2112@users.noreply.github.com>
2020-04-12 01:02:07 -0400

    minor restructures
    whitespace
    script tweaks

--- script.js
+++ script.js
@@ -21,10 +21,10 @@ function manageRadios(sel) {
 function calculatePills() {
 	let toe = 3,
 		antiSquat = 1,
-		deg = 0,
+		arm = ["c","d"],
 		pos1 = "_out",
 		pos2 = "down_",
-		arm = ["c","d"],
+		deg = 0,
 		i = 0;
 	//first pass C arm, second pass D arm
 	while (arm[i]) {
@@ -34,14 +34,15 @@ function calculatePills() {
 		}
 		let rbID = document.querySelector("input[name='" + arm[i] + "1']:checked").value;
 		let val = parseFloat(document.querySelector("input[name='" + arm[i] + "_degree']:checked").value);
+		//calculate toe
 		deg = val;
-		if (rbID.includes("_center")) {deg = 0;}
-		if (rbID.includes(pos1)) {deg *= -1;}
+		if (rbID.includes("_center")) { deg = 0; }
+			else { if (rbID.includes(pos1)) {deg *= -1;} }
 		toe += deg;
-		//calculate anti-squat
+		//calculate antisquat
 		deg = val;
-		if (rbID.includes("center_")) {deg = 0;}
-		if (rbID.includes(pos2)) {deg *= -1;}
+		if (rbID.includes("center_")) { deg = 0; }
+			else { if (rbID.includes(pos2)) {deg *= -1;} }
 		antiSquat += deg;
 		i++;
 	}
@@ -54,6 +55,19 @@ function doPillFuncs(inArm) {
 }
 
 //oil mixing
+function resetViscosities() {
+	document.querySelector("input[id = 'viscA']").value = "";
+	document.querySelector("input[id = 'viscB']").value = "";
+	document.querySelector("input[id = 'target']").value = "";
+}
+function resetResults() {
+	document.querySelector(".result[id = 'percentA']").innerHTML = "---";
+	document.querySelector(".result[id = 'percentB']").innerHTML = "---";
+}
+function resetAll() {
+	resetViscosities();
+	resetResults();
+}
 function validate(n) {
 	let result = true,
 		i = 0;
@@ -72,28 +86,23 @@ function inRange(a, b, c) {
 	return c > min && c < max;
 };
 function calculateOil() {
-	let viscA = document.getElementById("viscA").value,
-		viscB = document.getElementById("viscB").value,
-		target = document.getElementById("target").value;
-	if (!validate([viscA, viscB, target])) {
-		alert("All viscosity entries must be: \n     - A valid integer or float. \n     - Greater than zero.");
-	} else {
+	let viscA = document.querySelector("input[id = 'viscA']").value;
+		viscB = document.querySelector("input[id = 'viscB']").value;
+		target = document.querySelector("input[id = 'target']").value;
+	if (validate([viscA, viscB, target])) {
 		if (inRange(viscA, viscB, target)) {
 			let partA = ( (Math.log(target) - Math.log(viscB)) / (Math.log(viscA) - Math.log(viscB)) ),
 				partB = 1 - partA;
-			document.getElementById("percentA").innerHTML = Math.round(partA*100);
-			document.getElementById("percentB").innerHTML = Math.round(partB*100);
+			document.querySelector(".result[id = 'percentA']").innerHTML = Math.round(partA*100);
+			document.querySelector(".result[id = 'percentB']").innerHTML = Math.round(partB*100);
 		} else {
+			resetResults();
 			alert("Target viscosity must fall between the viscosities of fluid A and fluid B!");
 		}
+	} else {
+		resetResults();
+		alert("All viscosity entries must be: \n     - A valid integer or float. \n     - Greater than zero.");
 	}
-}
-function reset() {
-	document.querySelector("input[id = 'viscA']").value = "";
-	document.querySelector("input[id = 'viscB']").value = "";
-	document.querySelector("input[id = 'target']").value = "";
-	document.querySelector(".result[id = 'percentA']").innerHTML = "---";
-	document.querySelector(".result[id = 'percentB']").innerHTML = "---";
 }
 
 //navigation
